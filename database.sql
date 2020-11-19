85e283f704fe3b3a8eb9d5f05651bdaf60830952
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2020-11-19 13:33:08 -0600

    set up database

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE koalas (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR (20) NOT NULL,
+"gender" VARCHAR (80) NOT NULL,
+"age" INTEGER NOT NULL,
+"ready_to_transfer" VARCHAR (2) NOT NULL,
+"notes" VARCHAR (80)
+);
+
+INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+VALUES ('Scotty', 'M', '4', 'Y', 'Born in Guatemala'),
+	('Jean', 'F', '5', 'Y', 'Allergic to lots of Lava'),
+	('Ororo', 'F', '7', 'N', 'Loves listening to Paula (Abdul)'),
+	('Logan', 'M', '15', 'N', 'Loves the sauna'),
+	('Charlie', 'M', '9', 'Y', 'Favorite band is Nirvana'),
+	('Betsy', 'F', '4', 'Y', 'Has a pet iguana');

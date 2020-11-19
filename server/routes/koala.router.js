07fbbee88736375776e5bf4f3a6d63100486ebc6
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2020-11-19 16:19:59 -0600

    GET and POST

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,10 +5,35 @@ const koalaRouter = express.Router();
 
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koalas";';
+    pool.query(queryText).then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('error getting books', error);
+            res.sendStatus(500);
+        });
+});
 
 // POST
+koalaRouter.post('/', (req, res) => {
+    console.log(req.body);
+    let koalaToAdd = req.body;
+    let queryText = `
+    INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+    VALUES ($1, $2, $3, $4, $5);
+    `;
+    pool.query(queryText, [koalaToAdd.name, koalaToAdd.gender, koalaToAdd.age, koalaToAdd.ready_to_transfer, koalaToAdd.notes])
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
 
+});
 
 // PUT
 
